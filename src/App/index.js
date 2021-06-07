0c5657489fd8854166350703d781419eec83daa3
Chris Meffley <cmeffley@gmail.com>
2021-06-07 19:38:52 -0400

    started auth, getting development branch pushed

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,34 @@
-import React, { useState } from 'react';
-// import firebase from 'firebase';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 import Routes from '../helpers/Routes';
 import NavBar from '../components/NavBar';
 
 function App() {
-  // When you set up firebase add setUser method and change useState to null.
-  const [user] = useState(false);
+  const [coach, setCoach] = useState(false);
 
-  // Checking for authenticated users. You must set up firebase authentication for this to work!
-  // useEffect(() => {
-  //   firebase.auth().onAuthStateChanged((authed) => {
-  //     if (authed) {
-  //       const userInfoObject = {
-  //         fullName: authed.displayName,
-  //         profileImage: authed.photoURL
-  //         username: authed.email.split('@gmail.com')[0],
-  //         uid: authed.uid
-  //       };
-  //       setUser(userInfoObject);
-  //     } else if (user || user === null) {
-  //       setUser(false);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObject = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          username: authed.email.split('@gmail.com')[0],
+          uid: authed.uid
+        };
+        setCoach(userInfoObject);
+      } else if (coach || coach === null) {
+        setCoach(false);
+      }
+    });
+  }, []);
 
   return (
     <div className='App'>
        <Router>
-        <NavBar user={user}/>
-        <Routes user={user}/>
+        <NavBar coach={coach}/>
+        <Routes coach={coach}/>
       </Router>
     </div>
   );
