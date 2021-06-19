46e6a86f8720a060280a6bd8ea6c0559ef66631e
Chris Meffley <cmeffley@gmail.com>
2021-06-18 21:28:23 -0400

    wrote the rest of the needed promise calls

--- src/helpers/data/quickWorkoutsData.js
+++ src/helpers/data/quickWorkoutsData.js
@@ -9,4 +9,33 @@ const getQuickWorkouts = () => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-export default getQuickWorkouts;
+const addQuickWorkout = (quickWorkout) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/quickWorkouts.json`, quickWorkout)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/quickWorkouts/${response.data.name}.json`, body)
+        .then(() => {
+          getQuickWorkouts().then((quickWorkArray) => resolve(quickWorkArray));
+        });
+    })
+    .catch((error) => reject(error));
+});
+
+const editQuickWorkout = (quickWorkout) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/quickWorkouts/${quickWorkout.firebaseKey}.json`, quickWorkout)
+    .then(() => getQuickWorkouts()).then((array) => resolve(array))
+    .catch((error) => reject(error));
+});
+
+const deleteQuickWorkout = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/quickWorkouts/${firebaseKey}.json`)
+    .then(() => getQuickWorkouts().then((array) => resolve(array)))
+    .catch((error) => reject(error));
+});
+
+export {
+  getQuickWorkouts,
+  addQuickWorkout,
+  editQuickWorkout,
+  deleteQuickWorkout
+};
